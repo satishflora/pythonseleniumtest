55a758e1551a89223b9561c5bf84d22eba3366ba
satishtrantor <satish.kumar@trantorinc.com>
2022-03-01 23:20:17 +0530

    selenium code added

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print ("hello");
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def test_verify_application_open_successfully():
+    try:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.maximize_window()
+        driver.get("https://www.google.com")
+        # Getting current URL source code
+        get_title = driver.title
+        # Printing the title of this URL
+        print(get_title)
+        assert driver.title == "Google"
+    except Exception as e:
+        print(e)
+    finally:
+        driver.quit()
+
+        driver.quit()
